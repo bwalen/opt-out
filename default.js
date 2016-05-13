90f47b43bab88fa3d00d1a9e11e306ce2ef18283
bwalen <bwalen@hotmail.com>
2016-05-13 16:29:21 -0700

    #2 Have been working on methods to filter out dom elements that contain forbidden strings.

--- default.js
+++ default.js
@@ -1,32 +1,43 @@
 var listOfItems = ["trump", "bieber", "Bieber", "Trump", "Kanye", "kanye", "Kardashian", "kardashian","TRUMP"];
-var allPs = document.getElementsByTagName("p");
-var allSpans = document.getElementsByTagName("span");
-var allLinks = document.getElementsByTagName("a");
-console.log(searchElements(allPs,listOfItems));
-console.log(searchElements(allSpans,listOfItems));
-console.log(searchElements(allLinks,listOfItems));
-removeAllElements(searchElements(allLinks,listOfItems));
-removeAllElements(searchElements(allSpans,listOfItems));
-
+//var allDivs = document.getElementsByTagName("div");
+removeAllElements(searchElements(document.getElementsByTagName("p"),listOfItems));
+removeAllElements(searchElements(document.getElementsByTagName("a"),listOfItems));
+removeAllElements(searchElements(document.getElementsByTagName("span"),listOfItems));
+removeAllElements(searchElements(document.getElementsByTagName("li"), listOfItems));
+//removeAllElements(searchElements(allDivs,listOfItems));
+searchPictureNames(listOfItems);
 
 function searchElements(arrayOfItems, listOfItems){
   var cleanedList = [];
   for(var j = 0; j < listOfItems.length; j++){
     for(var i = 0; i < arrayOfItems.length; i++){
-      if(s.include(arrayOfItems[i].textContent,listOfItems[j])){
+      if(s.include(arrayOfItems[i].innerText,listOfItems[j])){
         cleanedList.push(arrayOfItems[i]);
       }
     }
   }
-  return (cleanedList);
+  return cleanedList;
 }
 
-function removeAllElements(listOfElements){
-  for(var i = 0; i < listOfElements.length; i++){
-    removeElement(listOfElements[i]);
+function searchPictureNames(listOfItems){
+  var cleanedList = [];
+  var picturesList = document.getElementsByTagName("img");
+  for(var j = 0; j < listOfItems.length; j++ ){
+    for (var i = 0; i < picturesList.length; i++){
+      if(s.include(picturesList[i].src,listOfItems[j])){
+        removeElement(picturesList[i]);
+      }
+    }
   }
 }
 
+function removeAllElements(listOfElements){
+  if(listOfElements.length > 0){
+    for(var i = 0; i < listOfElements.length; i++){
+      removeElement(listOfElements[i]);
+    }
+  }
+}
 
 function removeElement(element){
   if(element.parentNode){
