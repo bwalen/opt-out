8902085dfe530ea83df58ca2d436faf4cef2d516
bwalen <bwalen@hotmail.com>
2016-05-30 15:29:46 -0700

    #2 Updated to fix issue where imbedded tweets were not being scanned and removed.

--- default.js
+++ default.js
@@ -19,7 +19,7 @@ function watcher(){
 }
 
 function scanPage(){
-  removeAllElements(document.querySelectorAll("p,li,a,span,article,h1,h2,h3,h4,h5,img"));
+  removeAllElements(document.querySelectorAll("p,li,a,span,article,h1,h2,h3,h4,h5,img,blockquote"));
   removeAllElements(processDivs(document.getElementsByTagName("div")));
 }
 
